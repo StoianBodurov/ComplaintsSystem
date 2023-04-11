d70e20c6a4f221e8fa09b7edfc4533b0a411f03a
Stoian Bodurov <stoian.bodurov@abv.bg>
2023-04-11 17:41:26 +0300

    Add Wise-Service

---
+++ services/wise.py
@@ -0,0 +1,92 @@
+import json
+import uuid
+from decouple import config
+from fastapi import HTTPException
+import requests
+
+
+class WiseService:
+    def __init__(self):
+        self.main_url = config('WISE_URL')
+        self.headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {config("WISE_TOKEN")}'
+        }
+        self.profile_id = self._get_profile_id()
+
+    def _get_profile_id(self):
+        url = self.main_url + '/v1/profiles'
+        resp = requests.get(self.main_url, headers=self.headers)
+
+        if resp.status_code == 200:
+            resp = resp.json()
+            return [a['id'] for a in resp if a['type'] == 'personal'][0]
+        else:
+            raise HTTPException(status_code=500, detail='Payment provider is not available at the moment')
+
+    def create_quote(self, amount):
+        url = self.main_url + '/v2/quotes'
+        data = {
+            'sourceCurrency': 'EUR',
+            'targetCurrency': 'BGN',
+            'targetAmount': amount,
+            'profile': self.profile_id
+        }
+        resp = requests.post(url, headers=self.headers, data=json.dumps(data))
+
+        if resp.status_code == 200:
+            resp = resp.json()
+            return resp['id']
+        else:
+            raise HTTPException(status_code=500, detail='Payment provider is not available at the moment')
+
+    def create_recipient_account(self, full_name, iban):
+        url = self.main_url + '/v1/accounts'
+        data = {
+            'currency': 'EUR',
+            'type': 'iban',
+            'profile': self.profile_id,
+            'accountHolderName': full_name,
+            'legalType': 'PRIVATE',
+            'details': {'iban': iban}
+        }
+
+        resp = requests.post(url, headers=self.headers, data=json.dumps(data))
+
+        if resp.status_code == 200:
+            resp = resp.json()
+            return resp['id']
+
+        else:
+            raise HTTPException(status_code=500, detail='Payment provider is not available at the moment')
+
+    def create_transfer(self, target_account_id, quote_id):
+        customer_transaction_id = str(uuid.uuid4())
+
+        url = self.main_url + '/v1/transfers'
+        data = {
+            'targetAccount': target_account_id,
+            'quoteUuid': quote_id,
+            'customerTransactionId': customer_transaction_id,
+            'details': {}
+        }
+
+        resp = requests.post(url, headers=self.headers, data=json.dumps(data))
+
+        if resp.status_code == 200:
+            resp = resp.json()
+            return resp['id']
+        else:
+            raise HTTPException(status_code=500, detail='Payment provider is not available at the moment')
+
+    def fund_transfer(self, transfer_id):
+        url = self.main_url + f'/v3/profiles/{self.profile_id}/transfers/{transfer_id}/payments'
+        resp = requests.post(url, headers=self.headers)
+
+        if resp.status_code == 200:
+            resp = resp.json()
+            return resp['id']
+        else:
+            raise HTTPException(status_code=500, detail='Payment provider is not available at the moment')
+
+
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import boto3
 from decouple import config
+import requests
 
 # s3 = boto3.client('s3', aws_access_key_id=config('AWS_ACCESS_KEY'), aws_secret_access_key=config('AWS_SECRET'))
 #
@@ -29,34 +30,51 @@ from decouple import config
 # rs = delete_photo(key)
 # print(rs)
 
-secret_key_id = config('AWS_ACCESS_KEY')
-secret_key_access = config('AWS_SECRET')
-ses = boto3.client(
-    'ses',
-    region_name=config('SES_REGION'),
-    aws_access_key_id=secret_key_id,
-    aws_secret_access_key=secret_key_access
-)
+# secret_key_id = config('AWS_ACCESS_KEY')
+# secret_key_access = config('AWS_SECRET')
+# ses = boto3.client(
+#     'ses',
+#     region_name=config('SES_REGION'),
+#     aws_access_key_id=secret_key_id,
+#     aws_secret_access_key=secret_key_access
+# )
+#
+#
+# def send_email(subject, to_addresses, text_data):
+#     body = {}
+#     body.update({'Text': {'Data': text_data, 'Charset': 'UTF-8'}})
+#     try:
+#         ses.send_email(
+#             Source=config('SES_SOURCE_EMAIL'),
+#             Destination={
+#                 'ToAddresses': to_addresses,
+#                 'CcAddresses': [],
+#                 'BccAddresses': []
+#             },
+#             Message={
+#                 'Subject': {'Data': subject, 'Charset': 'UTF-8'},
+#                 'Body': body
+#             }
+#         )
+#     except Exception as ex:
+#         raise ex
+#
+#
+# send_email('Hello aws', ['stoian.bodurov@gmail.com'], 'My first email')
+
+main_url = config('WISE_URL') + '/v1/profiles'
+headers = {
+    'Content-Type': 'application/json',
+    'Authorization': f'Bearer {config("WISE_TOKEN")}'
+}
 
 
-def send_email(subject, to_addresses, text_data):
-    body = {}
-    body.update({'Text': {'Data': text_data, 'Charset': 'UTF-8'}})
-    try:
-        ses.send_email(
-            Source=config('SES_SOURCE_EMAIL'),
-            Destination={
-                'ToAddresses': to_addresses,
-                'CcAddresses': [],
-                'BccAddresses': []
-            },
-            Message={
-                'Subject': {'Data': subject, 'Charset': 'UTF-8'},
-                'Body': body
-            }
-        )
-    except Exception as ex:
-        raise ex
+def get_profile_id(url, h):
+    resp = requests.get(url, headers=h)
+
+    if resp.status_code == 200:
+        resp = resp.json()
+        return [a['id'] for a in resp if a['type'] == 'personal'][0]
 
 
-send_email('Hello aws', ['stoian.bodurov@gmail.com'], 'My first email')
+print(get_profile_id(main_url, headers))
